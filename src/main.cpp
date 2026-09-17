1512b3c9e4518c7ba15ce0c52c1a3e809e409f47
Kairav <kairavdutta@gmail.com>
2026-09-17 23:47:42 +0530

    fixed main file error and removed the 'include/' directory

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-#include <common/definitions.hpp>
-#include <common/types.hpp>
-#include <system/process.hpp>
-#include <terminal/pane.hpp>
+#include "common/definitions.hpp"
+#include "common/types.hpp"
+#include "pane/pane.hpp"
+#include "system/process.hpp"
 
 int main(int /* argc */, const char * /* argv */[]) { return 0; }
